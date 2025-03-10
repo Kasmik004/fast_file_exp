49c731f7e5387f97351db7731421b342631c7370
Kasmik004 <rkasmik@gmail.com>
2025-03-10 13:56:04 +0545

    open_files and have subfolder section

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,18 @@ use crossterm::{
     execute,
     terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode},
 };
+use ratatui::widgets::Block;
 use ratatui::{
     Terminal,
     backend::CrosstermBackend,
     layout::{Constraint, Direction, Layout},
-    widgets::{Block, Borders, List, ListItem},
+    widgets::{Borders, List, ListItem},
 };
-use std::path::Path;
+
+use std::{
+    error::Error,
+    path::{Path, PathBuf},
+};
 
 use open;
 use std::{fs, io};
@@ -24,13 +29,56 @@ use walkdir::WalkDir;
 //     }
 // }
 
-fn list_files(path: &str) -> Vec<String> {
-    let depth = 1;
-    WalkDir::new(path)
-        .min_depth(depth)
-        .max_depth(depth)
-        .into_iter()
-        .filter_map(|e| e.ok().map(|entry| entry.path().display().to_string()))
+struct App {
+    folders: Vec<PathBuf>,
+    files: Vec<PathBuf>,
+    selected_folder: usize,
+    selected_file: usize,
+    current_dir: PathBuf,
+}
+
+impl App {
+    fn new(initial_path: &str) -> Self {
+        let path = PathBuf::from(initial_path);
+        let mut folders = list_folders(&path);
+        let mut files = list_files(&path);
+        App {
+            folders,
+            files,
+            selected_folder: 0,
+            selected_file: 0,
+            current_dir: path,
+        }
+    }
+
+    fn change_dir(&mut self, dir: PathBuf) {
+        if dir.is_dir() {
+            self.current_dir = dir;
+            self.folders = list_folders(&self.current_dir);
+            self.files = list_files(&self.current_dir);
+            self.selected_folder = 0;
+            self.selected_file = 0;
+        }
+    }
+}
+
+fn list_folders(path: &Path) -> Vec<PathBuf> {
+    fs::read_dir(path)
+        .unwrap()
+        .filter_map(|entry| {
+            let entry = entry.unwrap().path();
+            if entry.is_dir() { Some(entry) } else { None }
+        })
+        .collect()
+}
+
+fn list_files(path: &Path) -> Vec<PathBuf> {
+    fs::read_dir(path)
+        .unwrap()
+        .filter_map(|entry| {
+            let entry = entry.unwrap().path();
+            if entry.is_file() { Some(entry) } else { None }
+        })
         .collect()
 }
 
@@ -45,61 +93,124 @@ fn test() {
     }
 }
 
-fn main() -> io::Result<()> {
+fn main() -> io::Result<(), Box<dyn Error>> {
     // let dir = "."; //directory path
     // // list_files(dir);
     // test();
-    enable_raw_mode().unwrap();
+    enable_raw_mode().unwrap_or_else(|err| {
+        eprintln!("Failed to enable raw mode: {}", err);
+        std::process::exit(1);
+    });
     let mut stdout = io::stdout();
     execute!(stdout, EnterAlternateScreen).unwrap();
 
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend).unwrap();
 
-    let mut files = list_files(".");
-    let mut selected = 0;
+    let mut app = App::new(".");
+
+    // let mut files = list_files(".");
+    // let mut selected = 0;
+
+    // if files.is_empty() {
+    //     eprintln!("No files found");
+    //     disable_raw_mode().unwrap(); // Restore terminal
+    //     execute!(io::stdout(), LeaveAlternateScreen).unwrap();
+    //     return Ok(());
+    // }
 
     loop {
-        terminal.draw(|f| {
-            let size: ratatui::prelude::Rect = f.size();
-            let chunks = Layout::default()
-                .direction(Direction::Vertical)
-                .constraints([Constraint::Percentage(100)].as_ref())
-                .split(size);
-            let items: Vec<ListItem> = files
-                .iter()
-                .enumerate()
-                .map(|(i, file)| {
-                    if i == selected {
-                        ListItem::new(format!("> {}", file))
-                    } else {
-                        ListItem::new(format!("  {}", file))
-                    }
-                })
-                .collect();
+        terminal
+            .draw(|f| {
+                let size: ratatui::prelude::Rect = f.size();
+                let chunks = Layout::default()
+                    .direction(Direction::Horizontal)
+                    .constraints([Constraint::Percentage(30), Constraint::Percentage(70)].as_ref())
+                    .split(size);
+                // let items: Vec<ListItem> = files
+                //     .iter()
+                //     .enumerate()
+                //     .map(|(i, file)| {
+                //         if i == selected {
+                //             ListItem::new(format!("> {}", file))
+                //         } else {
+                //             ListItem::new(format!("  {}", file))
+                //         }
+                //     })
+                //     .collect();
 
-            let list =
-                List::new(items).block(Block::default().title("Files").borders(Borders::ALL));
-            f.render_widget(list, chunks[0]);
-            f.set_cursor(chunks[0].x + 1, chunks[0].y + selected as u16 + 1);
-        });
+                let folder_items: Vec<ListItem> = app
+                    .folders
+                    .iter()
+                    .enumerate()
+                    .map(|(i, folder)| {
+                        let folder_name = folder.file_name().unwrap().to_string_lossy();
+                        if i == app.selected_folder {
+                            ListItem::new(format!("> {}", folder_name))
+                        } else {
+                            ListItem::new(format!("  {}", folder_name))
+                        }
+                    })
+                    .collect();
+
+                let folder_list = List::new(folder_items)
+                    .block(block::default().title("Folders").borders(Borders::ALL));
+                f.render_widget(folder_list, chunks[0]);
+
+                let files_list: Vec<ListItem> = app
+                    .files
+                    .iter()
+                    .enumerate()
+                    .map(|(i, file)| {
+                        let file_name: std::borrow::Cow<'_, str> =
+                            file.file_name().unwrap().to_string_lossy();
+                        if i == app.selected_file {
+                            ListItem::new(format!("> {}", file_name))
+                        } else {
+                            ListItem::new(format!("  {}", file_name))
+                        }
+                    })
+                    .collect();
+
+                let file_list = List::new(files_list)
+                    .block(block::default().title("Files").borders(Borders::ALL));
+                f.render_widget(file_list, chunks[1]);
+                f.set_cursor(chunks[1].x + 1, chunks[1].y + app.selected_file as u16 + 1);
+
+                // let list =
+                //     List::new(items).block(Block::default().title("Files").borders(Borders::ALL));
+                // f.render_widget(list, chunks[0]);
+                // f.set_cursor(chunks[0].x + 1, chunks[0].y + selected as u16 + 1);
+            })
+            .unwrap();
 
         if let event::Event::Key(key) = event::read().unwrap() {
             match key.code {
                 KeyCode::Down => {
-                    if selected < files.len() - 1 {
-                        selected += 1;
+                    if app.selected_file < app.files.len() - 1 {
+                        app.selected_file += 1;
+                        // selected += 1;
                     }
                 }
                 KeyCode::Up => {
-                    if selected > 0 {
-                        selected -= 1;
+                    if app.selected_file > 0 {
+                        app.selected_file -= 1;
+                    }
+                }
+                KeyCode::Right => {
+                    if app.selected_folder < app.folders.len() - 1 {
+                        app.selected_folder += 1;
+                    }
+                }
+                KeyCode::Left => {
+                    if app.selected_folder > 0 {
+                        app.selected_folder -= 1;
                     }
                 }
                 KeyCode::Enter => {
-                    if let Some(file) = files.get(selected) {
-                        if let Err(err) = open::that(file) {
-                            eprintln!("Failed to open file: {}", err);
+                    if let Some(file) = app.files.get(app.selected_file) {
+                        if let Err(e) = open::that(file) {
+                            eprintln!("Failed to open file: {}", e);
                         }
                     }
                 }
